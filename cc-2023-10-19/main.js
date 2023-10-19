71676a3272e954a8aaf39e334ead3f4d6832f354
Jordan Russo <jresiduum@gmail.com>
2023-10-19 06:51:42 -0400

    Completed coding challenge for 2023-10-19

---
+++ cc-2023-10-19/main.js
@@ -0,0 +1,32 @@
+// url: https://leetcode.com/problems/sleep/
+
+// positive integer (milies)
+
+// return promise sleeps for milis ms, reesolve to anything.
+
+// Example 1:
+// Input: millis = 100
+// Output: 100
+// Explanation: It should return a promise that resolves after 100ms.
+// let t = Date.now();
+// sleep(100).then(() => {
+//   console.log(Date.now() - t); // 100
+// });
+
+// Example 2:
+// Input: millis = 200
+// Output: 200
+// Explanation: It should return a promise that resolves after 200ms.
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
