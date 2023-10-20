c88f052d52c6f4bf6cb53e6e8224cf25514bced4
Jordan Russo <jresiduum@gmail.com>
2023-10-20 05:21:32 -0400

    Completed coding challenge for 2023-10-20

---
+++ cc-2023-10-20/main.js
@@ -0,0 +1,90 @@
+// Given an async function, and time limit
+
+// Return a new function that takes arguments to the time limited functino
+
+// If it completes in time should resolve with the result
+
+// If it exceeds the time limit function should reject with "Time Limit Exceeded"
+    // Other errors should still reject as well.
+
+// Example 1:
+
+// Input: 
+// fn = async (n) => { 
+//   await new Promise(res => setTimeout(res, 100)); 
+//   return n * n; 
+// }
+// inputs = [5]
+// t = 50
+// Output: {"rejected":"Time Limit Exceeded","time":50}
+// Explanation:
+// const limited = timeLimit(fn, t)
+// const start = performance.now()
+// let result;
+// try {
+//    const res = await limited(...inputs)
+//    result = {"resolved": res, "time": Math.floor(performance.now() - start)};
+// } catch (err) {
+//    result = {"rejected": err, "time": Math.floor(performance.now() - start)};
+// }
+// console.log(result) // Output
+
+// The provided function is set to resolve after 100ms. However, the time limit is set to 50ms. It rejects at t=50ms because the time limit was reached.
+// Example 2:
+
+// Input: 
+// fn = async (n) => { 
+//   await new Promise(res => setTimeout(res, 100)); 
+//   return n * n; 
+// }
+// inputs = [5]
+// t = 150
+// Output: {"resolved":25,"time":100}
+// Explanation:
+// The function resolved 5 * 5 = 25 at t=100ms. The time limit is never reached.
+// Example 3:
+
+// Input: 
+// fn = async (a, b) => { 
+//   await new Promise(res => setTimeout(res, 120)); 
+//   return a + b; 
+// }
+// inputs = [5,10]
+// t = 150
+// Output: {"resolved":15,"time":120}
+// Explanation:
+// ​​​​The function resolved 5 + 10 = 15 at t=120ms. The time limit is never reached.
+// Example 4:
+
+// Input: 
+// fn = async () => { 
+//   throw "Error";
+// }
+// inputs = []
+// t = 1000
+// Output: {"rejected":"Error","time":0}
+// Explanation:
+// The function immediately throws an error.
+
+// Promise races resolve or reject the first settled promise
+// Make a promise that rejects with "time limit exceeded" after t, ms.
+// Make another promise by calling the given function with arguments
+// Return whichever promise comes first
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+	return async function(...args) {
+        const timeout = new Promise((resolve, reject) => setTimeout(() => {reject('Time Limit Exceeded')}, t));
+        return Promise.race([fn(...args), timeout])
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
