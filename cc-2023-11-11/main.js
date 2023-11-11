80c0de1b03451bb73951b780e6d12c9d7777dea3
Jordan Russo <jresiduum@gmail.com>
2023-11-11 09:34:18 -0500

    Completed coding challenge for 2023-11-11

---
+++ cc-2023-11-11/main.js
@@ -0,0 +1,49 @@
+// url: https://leetcode.com/problems/allow-one-function-call/
+
+// Given a callback fn, return a new function that will return the old function's call only once.
+// Additional calls should return undefined
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+// Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+
+// use closure to keep state of the function
+// initialied as not called
+// when called change the state of call
+// doesn't return anything when called
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false
+	return function(...args){
+        if(!called){
+            called = true
+            return fn(...args)
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
