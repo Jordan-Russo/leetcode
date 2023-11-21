91cebf816bde516e04d2dd9a4d454e76566c209a
Jordan Russo <jresiduum@gmail.com>
2023-11-21 13:25:35 -0500

    Completed coding challenge for 2023-11-21

---
+++ cc-2023-11-21/main.js
@@ -0,0 +1,17 @@
+// url: https://leetcode.com/problems/reverse-string/
+
+// Given an array with elements that are strings
+
+// Return the array in reverse (mutated in place)
+
+// Example 1:
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+// Example 2:
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+var reverseString = function(s) {
+    return s.reverse();
+};
