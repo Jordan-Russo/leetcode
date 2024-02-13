752fc1dc92233a5bdadb1d8bc8874ab3617c45bb
Jordan Russo <jresiduum@gmail.com>
2024-02-13 08:22:07 -0500

    Completed coding challenge for 2024-02-13

---
+++ cc-2024-02-13/main.js
@@ -0,0 +1,9 @@
+// url: https://leetcode.com/problems/sort-by/
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b))
+};
