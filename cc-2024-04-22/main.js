44af0b5f1df35245aa573800df463c621525501e
Jordan Russo <jresiduum@gmail.com>
2024-04-22 00:44:28 -0400

    Completed coding challenge for 2024-04-22

---
+++ cc-2024-04-22/main.js
@@ -0,0 +1,72 @@
+// url: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+// You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+// The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+// For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ 
+
+// Example 1:
+
+
+// Input: head = [1,3,4,7,1,2,6]
+// Output: [1,3,4,1,2,6]
+// Explanation:
+// The above figure represents the given linked list. The indices of the nodes are written below.
+// Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+// We return the new list after removing this node. 
+// Example 2:
+
+
+// Input: head = [1,2,3,4]
+// Output: [1,2,4]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+// Example 3:
+
+
+// Input: head = [2,1]
+// Output: [2]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+// Node 0 with value 2 is the only node remaining after removing node 1.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function(head) {
+    // find length of list
+    let curr = head
+    let length = 0
+    while(curr){
+        length++
+        curr = curr.next
+    }
+    if(length === 1){
+        head = null
+        return head
+    }
+    // find the number of the middle node
+    let middle = Math.trunc(length / 2)
+    // if length 1 assign it to null
+    curr = head
+    // loop until you're at the number before the middle
+        // set that node's next node to the middle's next node.
+    // while loop to get to one before middle
+    while(middle !== 1){
+        curr = curr.next
+        middle--
+    }
+    curr.next = curr.next.next
+    return head
+};
