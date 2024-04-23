b731c910a7163873f0a3a8cc21ce12aff34bef48
Jordan Russo <jresiduum@gmail.com>
2024-04-23 09:29:44 -0400

    Completed coding challenge for 2024-04-23

---
+++ cc-2024-04-23/main.js
@@ -0,0 +1,43 @@
+// url: https://leetcode.com/problems/remove-linked-list-elements/
+// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+// Example 1:
+
+
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+// Example 2:
+
+// Input: head = [], val = 1
+// Output: []
+// Example 3:
+
+// Input: head = [7,7,7,7], val = 7
+// Output: []
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    while(head?.val === val){
+        head = head.next
+    }
+    let curr = head
+    while(curr){
+        let next = curr.next
+        while(next?.val === val){
+            next = next.next
+        }
+        curr.next = next
+        curr = curr.next
+    }
+    return head
+};
