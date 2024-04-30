c96761e6ce57dcf5f5ed4887413a63696f797757
Jordan Russo <jresiduum@gmail.com>
2024-04-30 09:52:58 -0400

    Completed coding challenge for 2024-04-30

---
+++ cc-2024-04-30/main.js
@@ -0,0 +1,39 @@
+// url: https://leetcode.com/problems/count-square-sum-triples/description/
+// A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+// Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+ 
+
+// Example 1:
+
+// Input: n = 5
+// Output: 2
+// Explanation: The square triples are (3,4,5) and (4,3,5).
+
+// Example 2:
+
+// Input: n = 10
+// Output: 4
+// Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function(n) {
+    let count = 0
+    for(let i = 1; i <= n; i++){
+        for(let j = 1; j <= n; j++){
+            for(let k = 1; k <= n; k++){
+                const a = i
+                const b = j
+                const c = k
+                if(a * a + b * b === c * c){
+                    count++
+                }
+            }
+        }
+    }
+    return count
+};
