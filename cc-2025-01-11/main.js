fbbc0826fa91b493bdfb2034b929251c1e1eeb5a
Jordan Russo <jresiduum@gmail.com>
2025-01-11 00:00:16 -0500

    Completed coding challenge for 2025-01-11

---
+++ cc-2025-01-11/main.js
@@ -0,0 +1,49 @@
+// url: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+//  Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+// Return the decimal value of the number in the linked list.
+
+// The most significant bit is at the head of the linked list.
+
+ 
+
+// Example 1:
+
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+
+// Example 2:
+
+// Input: head = [0]
+// Output: 0
+
+ 
+
+// Constraints:
+
+//     The Linked List is not empty.
+//     Number of nodes will not exceed 30.
+//     Each node's value is either 0 or 1.
+
+
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+function getDecimalValue(head){
+    let binaryString = ''
+    let curr = head
+    while(curr){
+        binaryString += curr.val
+        curr = curr.next
+    }
+    return parseInt(binaryString, 2)
+};
