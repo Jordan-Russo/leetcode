4876196a7858b4a2b2e444fd776da471b87eef61
Jordan Russo <jresiduum@gmail.com>
2025-02-19 07:46:18 -0500

    Completed coding challenge for 2025-02-19

---
+++ cc-2025-02-19/main.js
@@ -0,0 +1,48 @@
+// url: https://leetcode.com/problems/interval-cancellation/
+// given a function, fn; an array of arguments, args; an interval time in ms, t
+// return a cancel function, cancelFn
+// when the cancel function gets called
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+function cancellable(fn, args, t){
+    // immediately, then at intervals
+    const call = () => fn(...args);
+    call();
+    const intervalID = setInterval(call, t);
+    // make a helper function
+    return () => clearInterval(intervalID);
+    // return cancel function
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *   
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)    
+ */
