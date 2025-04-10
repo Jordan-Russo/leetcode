7237df6a496c7ad33b103c4115ab08645864109b
Jordan Russo <jresiduum@gmail.com>
2025-04-10 00:00:09 -0400

    Completed coding challenge for 2025-04-10

---
+++ cc-2025-04-10/main.js
@@ -0,0 +1,55 @@
+// url: https://leetcode.com/problems/defuse-the-bomb/description/
+// You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
+
+// To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+
+//     If k > 0, replace the ith number with the sum of the next k numbers.
+//     If k < 0, replace the ith number with the sum of the previous k numbers.
+//     If k == 0, replace the ith number with 0.
+
+// As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+
+// Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
+/**
+ * @param {number[]} code
+ * @param {number} k
+ * @return {number[]}
+ */
+var decrypt = function(code, k) {
+    // create a result array
+    const result = [];
+    // loop through the code array
+    for(let i = 0; i < code.length; i++){
+        // initialize a sum at 0
+        let sum = 0;
+        // loop through the numbers in the k range from our position
+        const absK = Math.abs(k);
+        for(let j = 1; j <= absK; j++){
+            // add looped values to a sum
+            let pos = (i + j * Math.sign(k)) % code.length;
+            if(pos < 0){pos += code.length}
+            sum += code[pos];
+        }
+        // push the sum to the array
+        result.push(sum);
+    }
+    // return the result array
+    return result;
+};
+// Example 1:
+
+// Input: code = [5,7,1,4], k = 3
+// Output: [12,10,16,13]
+// Explanation: Each number is replaced by the sum of the next 3 numbers. The decrypted code is [7+1+4, 1+4+5, 4+5+7, 5+7+1]. Notice that the numbers wrap around.
+
+// Example 2:
+
+// Input: code = [1,2,3,4], k = 0
+// Output: [0,0,0,0]
+// Explanation: When k is zero, the numbers are replaced by 0. 
+
+// Example 3:
+
+// Input: code = [2,4,9,3], k = -2
+// Output: [12,5,6,13]
+// Explanation: The decrypted code is [3+9, 2+3, 4+2, 9+4]. Notice that the numbers wrap around again. If k is negative, the sum is of the previous numbers.
