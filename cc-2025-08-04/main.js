60b9a43d963b1b2422576d08986198cfe25b14f4
Jordan Russo <jresiduum@gmail.com>
2025-08-04 08:41:18 -0400

    Completed coding challenge for 2025-08-04

---
+++ cc-2025-08-04/main.js
@@ -0,0 +1,14 @@
+class MyHashMap{
+    constructor(){
+        this.map = new Map();
+    }
+    put(key, value){
+        this.map.set(key, value);
+    }
+    get(key){
+        return this.map.get(key) ?? -1;
+    }
+    remove(key){
+        this.map.delete(key);
+    }
+}
