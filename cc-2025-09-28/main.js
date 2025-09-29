725713c2781f929c6099b3985c009d2c63ede879
Jordan Russo <jresiduum@gmail.com>
2025-09-28 23:19:33 -0400

    Completed coding challenge for 2025-09-28

---
+++ cc-2025-09-28/main.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDiffInBST = function(root){
+    let minDiff = Infinity;
+    let prev;
+    recursive(root);
+    return minDiff;
+    function recursive(node){
+        if(!node){return}
+        if(node.left){
+            recursive(node.left);
+        }
+        if(prev !== undefined){
+            const diff = Math.abs(prev - node.val);
+            minDiff = Math.min(diff, minDiff);
+        }
+        prev = node.val;
+        if(node.right){
+            recursive(node.right);
+        }
+    }
+    // in-order traversal
+    // go as left as possible
+    // visit node
+    // go as right as possible
+};
